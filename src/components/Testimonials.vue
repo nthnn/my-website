54843eee8efd265489f9553a6d26d0857fdf9599
Nathanne Isip <nathanneisip@gmail.com>
2025-05-03 10:20:44 +0800

    Vue component that contains testimonials about my services.

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="my-card p-4 bg-primary" id="experiences">
+        <div class="mt-2 mb-4">
+            <h3 class="d-block fw-bold" id="testimonials">Testimonials</h3>
+
+            <div class="row pt-4">
+                <div class="col-lg-4 p-4" align="center">
+                    <a target="_blank" href="https://www.instagram.com/lady.selenee/">
+                        <img src="@/assets/images/testimonials/ladyselene.png" class="w-50" />
+                    </a>
+
+                    <h4 class="pt-4 mb-0">Lady Selene</h4>
+                    <small>Graphic Artist of <a class="no-underline ic-link" href="https://j9n8.github.io/" target="_blank">J9N8</a></small>
+
+                    <hr/>
+                    <p>Working with Nathanne is an absolute pleasure. He is incredibly skilled and knowledgeable, and his attention to detail was unmatched. I was thoroughly impressed with the quality of his work and would recommend them to anyone in need of a talented and reliable developer.</p>
+                </div>
+
+                <div class="col-lg-4 p-4" align="center">
+                    <a target="_blank" href="https://www.linkedin.com/in/elijahjudson/">
+                        <img src="@/assets/images/testimonials/elijah.png" class="w-50" />
+                    </a>
+
+                    <h4 class="pt-4 mb-0">Elijah</h4>
+                    <small>Virtual Healthcare Operations</small>
+
+                    <hr/>
+                    <p>He is an excellent website developer and android app developer. He was able to work with tight deadlines, and can do more than what you expect him to do for you!<br/><br/>Anyone is lucky to have Nathanne on their side!</p>
+                </div>
+
+                <div class="col-lg-4 p-4" align="center">
+                    <a target="_blank" href="https://www.instagram.com/ngenetttt/">
+                        <img src="@/assets/images/testimonials/kenneth.png" class="w-50" />
+                    </a>
+
+                    <h4 class="pt-4 mb-0">Mark Kenneth</h4>
+                    <small>BS Computer Science Student</small>
+
+                    <hr/>
+                    <p>Nathanne is an outstanding developer who provided exceptional services for my project. His expertise and professionalism made the entire process a pleasure, and I would highly recommend him to anyone looking for a top-notch developer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
