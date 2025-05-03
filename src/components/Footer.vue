0e7c90215da580ab8f3d47b3b27a064971a1e9d4
Nathanne Isip <nathanneisip@gmail.com>
2025-05-03 10:18:14 +0800

    Footer component Vue file.

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <hr>
+
+    <div align="center">
+        <p>Crafted with &#x1F496; by <a target="_blank" href="https://github.com/nthnn">Nathanne Isip</a>.</p>
+    </div>
+</template>
