da140010a10eba464d21fb43ec424aff8c653932
Nathanne Isip <nathanneisip@gmail.com>
2025-05-03 10:13:38 +0800

    Main application source file in TypeScript.

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import "./assets/styles/bootstrap.min.css";
+import "./assets/styles/global.css";
+
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "fa-icons";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
