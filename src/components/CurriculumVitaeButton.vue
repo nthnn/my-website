97aeb38337a170d6b3507adfaa503cc3677f455c
Nathanne Isip <nathanneisip@gmail.com>
2025-05-03 10:20:24 +0800

    Button component for downloading my curriculum vitae.

---
+++ src/components/CurriculumVitaeButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <a target="_blank" href="/nthnn-resume.pdf" class="text-info text-decoration-none">
+        Curriculum Vitae
+        <i class="bi bi-arrow-up-right"></i>
+    </a>
+</template>
