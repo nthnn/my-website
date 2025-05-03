b078b9d51ed7179db8b79bd80c9b2a08c13f27a8
Nathanne Isip <nathanneisip@gmail.com>
2025-05-03 10:21:53 +0800

    Index homepage view file of my website.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import MeCard from "@/components/MeCard.vue";
+import MyExperiences from "@/components/MyExperiences.vue";
+import MyExpertise from "@/components/MyExpertise.vue";
+import Testimonials from "@/components/Testimonials.vue";
+</script>
+
+<template>
+    <MeCard />
+
+    <div class="p-lg-4">
+        <div align="center">
+            <div class="col-lg-11" align="left">
+                <div class="mt-2 mb-4 border-1 border-gray border-top border-bottom w-100 p-3 toc" align="center">
+                    <a href="#experiences" class="d-inline-block text-decoration-none text-uppercase"><i class="bi bi-briefcase"></i> <span class="desktop-only">Experiences</span></a>
+                    <a href="#expertise" class="d-inline-block text-decoration-none text-uppercase ms-4"><i class="bi bi-cup-hot"></i> <span class="desktop-only">Expertise</span></a>
+                    <a href="#testimonials" class="d-inline-block text-decoration-none text-uppercase ms-4"><i class="bi bi-trophy"></i> <span class="desktop-only">Testimonials</span></a>
+                </div>
+
+                <MyExperiences />
+                <MyExpertise />
+                <Testimonials />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.toc a {
+    color: var(--bs-gray-400) !important;
+}
+</style>
