4eab5b1f89506dba4111f9e6c9564982cfe3c698
Nathanne Isip <nathanneisip@gmail.com>
2025-06-27 04:26:12 +0800

    Fixed bug on toggleBurger() function implementation

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,7 @@ import TantanAI from "./components/TantanAI.vue";
 const route = useRoute();
 watch(
     () => route.fullPath,
-    async (_, __) => {
-        toggleBurger();
-        window.scrollTo(0, 0);
-    }
+    async (_, __) => window.scrollTo(0, 0)
 );
 
 document.addEventListener('contextmenu', (event) => {
