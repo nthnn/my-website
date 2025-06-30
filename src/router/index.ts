8d3535ed4bc220e73ba0ddeaa96c02cfe7ac2bb1
Nathanne Isip <nathanneisip@gmail.com>
2025-06-30 15:46:44 +0800

    Added article view page on router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import GalleryView from "@/views/GalleryView.vue";
 import ProjectsView from "@/views/ProjectsView.vue";
+import ArticleView from "@/views/ArticleView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: "/projects",
             name: "projects",
             component: ProjectsView
+        },
+        {
+            path: "/view",
+            name: "view",
+            component: ArticleView
         },
         {
             path: "/:catchAll(.*)",
