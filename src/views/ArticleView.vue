7bda833253834e2df296a6156f34551f681427c3
Nathanne Isip <nathanneisip@gmail.com>
2025-07-01 01:37:41 +0800

    Improved ArticleView page for showing contents of project article.

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -9,6 +9,9 @@ const route = useRoute();
 const router = useRouter();
 
 const projectContent = ref("");
+const projectTitle = ref("");
+const projectLink = ref("");
+
 const loadArticle = async (id: string | string[]) => {
     const articleId = Array.isArray(id) ? id[0] : id;
     if(!articleId || isNaN(Number(articleId))) {
@@ -22,11 +25,14 @@ const loadArticle = async (id: string | string[]) => {
             throw new Error(`Failed to fetch project data: ${response.status}`);
 
         const data = await response.json();
-        document.title = data.title;
+        projectTitle.value = document.title = data.title;
         projectContent.value = data.content;
 
+        if(data.link)
+            projectLink.value = data.link;
+
         setTimeout(() => {
-            if (document.querySelector(".main-section")) {
+            if(document.querySelector(".main-section")) {
                 baguetteBox.run(".main-section", {
                     animation: 'fadeIn',
                     noScrollBars: true,
@@ -51,7 +57,19 @@ watch(
 </script>
 
 <template>
-    <div>
-        <div class="container main-section" id="main-content" v-html="projectContent"></div>
+    <div class="row gx-0 mb-0 pb-0">
+        <div class="col-9 col-lg-8 mb-0 pb-0">
+            <h1 class="shimmer mt-2 mb-0 pb-0">{{ projectTitle }}</h1>
+        </div>
+
+        <div class="col-3 col-lg-4 mb-0 pb-0">
+            <div v-if="projectLink !== ''" class="w-100" align="right">
+                <a :href="projectLink" class="btn btn-info" target="_blank"><i class="bi bi-github"></i><span class="desktop-only"> View on GitHub</span></a>
+            </div>
+        </div>
     </div>
+    <hr/>
+
+    <div class="main-section" id="main-content" v-html="projectContent"></div>
+    <br/>
 </template>
