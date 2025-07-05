82757f1f4ce068cb8e7cf4c372bf57614c543468
Nathanne Isip <nathanneisip@gmail.com>
2025-07-05 09:47:26 +0800

    Invalid ID URL parameter handling in article view.

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, type Ref } from "vue";
 import { onBeforeRouteLeave, useRoute, useRouter } from "vue-router";
 
 import "baguettebox.js/dist/baguetteBox.min.css";
@@ -7,6 +7,7 @@ import baguetteBox from "baguettebox.js/dist/baguetteBox.min.js";
 
 import ProjectCard from "@/components/ProjectCard.vue";
 import { animateTitle } from "@/scripts/title";
+import { hideLoadingBar } from "@/scripts/loading";
 
 const route = useRoute();
 const router = useRouter();
@@ -15,6 +16,22 @@ const projectContent = ref("");
 const projectTitle = ref("");
 const projectLink = ref("");
 const randomProjects = ref([]);
+
+const isLoading: Ref<boolean> = ref(true);
+const hasError: Ref<boolean> = ref(false);
+const showArticleContent: Ref<boolean> = ref(false);
+
+const showArticle = () => {
+    isLoading.value = false;
+    hasError.value = false;
+    showArticleContent.value = true;
+};
+
+const showError = () => {
+    isLoading.value = false;
+    hasError.value = true;
+    showArticleContent.value = false;
+};
 
 const fetchAndSetRandomProjects = async(currentArticleId: string)=> {
     try {
@@ -46,14 +63,21 @@ const fetchAndSetRandomProjects = async(currentArticleId: string)=> {
 
         const shuffled = projectsInSameCategory.sort(()=> 0.5 - Math.random());
         randomProjects.value = shuffled.slice(0, 3);
+
+        hideLoadingBar(showArticle);
     }
     catch(error) {
         randomProjects.value = [];
+        hideLoadingBar(showError);
     }
 };
 
 let animateInterval: number = 0;
 const loadArticle = async (id: string | string[])=> {
+    isLoading.value = true;
+    hasError.value = false;
+    showArticleContent.value = false;
+
     const articleId = Array.isArray(id) ? id[0] : id;
     if(!articleId || isNaN(Number(articleId))) {
         router.push("/404");
@@ -89,12 +113,19 @@ const loadArticle = async (id: string | string[])=> {
     catch(error) {
         console.error("Error loading article:", error);
         router.push("/404");
+
+        hideLoadingBar(showError);
     }
 };
 
 watch(
     ()=> route.query.id,
     (newId)=> {
+        if(!parseInt(newId as string, 10)) {
+            hideLoadingBar(showError);
+            return;
+        }
+
         if(newId)
             loadArticle(newId as string);
     },
@@ -105,7 +136,7 @@ onBeforeRouteLeave(()=> clearInterval(animateInterval));
 </script>
 
 <template>
-    <div class="row gx-0 mb-0 pb-0">
+    <div v-if="showArticleContent" class="row gx-0 mb-0 pb-0">
         <div class="col-9 col-lg-8 mb-0 pb-0">
             <h1 class="shimmer mt-2 mb-0 pb-0">{{ projectTitle }}</h1>
         </div>
@@ -118,10 +149,23 @@ onBeforeRouteLeave(()=> clearInterval(animateInterval));
     </div>
     <hr/>
 
-    <div class="main-section" id="main-content" v-html="projectContent"></div>
-    <br/><hr/><br/>
+    <div v-if="isLoading" id="loading-section" class="d-block" align="center">
+        <br/>
+        <img src="/images/gear.png" class="rotating-gear mt-2" width="32" />
+        <p class="mt-3">Article is currently being loaded, please wait...</p>
+        <br/>
+    </div>
 
-    <template v-if="randomProjects.length > 0">
+    <div v-if="hasError" id="error-section" class="d-block" align="center">
+        <br/>
+        <p class="mt-3">Something went wrong while trying to load article.</p>
+        <br/>
+    </div>
+
+    <div v-if="showArticleContent" class="main-section" id="main-content" v-html="projectContent"></div>
+
+    <template v-if="!isLoading && randomProjects.length > 0">
+        <br/><hr/><br/>
         <h2 class="shimmer" align="center">You might also like</h2>
         <div class="row equal-cols m-0 p-0">
             <div
@@ -135,7 +179,8 @@ onBeforeRouteLeave(()=> clearInterval(animateInterval));
         </div>
         <br/>
     </template>
-    <template v-else>
+    <template v-else-if="!isLoading && !hasError && randomProjects.length === 0">
+        <br/><hr/><br/>
         <p class="mb-4 pb-3" align="center"><span class="shimmer">No other projects to recommend at the moment.</span></p>
     </template>
 </template>
