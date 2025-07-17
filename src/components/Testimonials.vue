deae57c3fbb2043e26a668e21c2a5684a62432de
Nathanne Isip <nathanneisip@gmail.com>
2025-07-18 01:14:46 +0800

    Testimonial images updated.

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -6,11 +6,11 @@
             <div class="row pt-4">
                 <div class="col-lg-4 p-4" align="center">
                     <a target="_blank" href="https://www.instagram.com/lady.selenee/">
-                        <img src="@/assets/images/testimonials/ladyselene.png" class="w-50" />
+                        <img src="@/assets/images/testimonials/ladyselene.png" class="rounded-circle w-50" />
                     </a>
 
                     <h4 class="pt-4 mb-0">Lady Selene</h4>
-                    <small>Graphic Artist of <a class="no-underline ic-link" href="https://j9n8.github.io/" target="_blank">J9N8</a></small>
+                    <small>Graphic Artist of <a class="text-decoration-none text-info ic-link" href="https://j9n8.github.io/" target="_blank">J9N8</a></small>
 
                     <hr/>
                     <p>Working with Nathanne is an absolute pleasure. He is incredibly skilled and knowledgeable, and his attention to detail was unmatched. I was thoroughly impressed with the quality of his work and would recommend them to anyone in need of a talented and reliable developer.</p>
@@ -18,26 +18,26 @@
 
                 <div class="col-lg-4 p-4" align="center">
                     <a target="_blank" href="https://www.linkedin.com/in/elijahjudson/">
-                        <img src="@/assets/images/testimonials/elijah.png" class="w-50" />
+                        <img src="@/assets/images/testimonials/elijah.png" class="rounded-circle w-50" />
                     </a>
 
-                    <h4 class="pt-4 mb-0">Elijah</h4>
-                    <small>Virtual Healthcare Operations</small>
+                    <h4 class="pt-4 mb-0">Elijah Judson</h4>
+                    <small>Legal Assistant at <a class="text-decoration-none text-info" href="https://mwpetersonlaw.com/" target="_blank">MW Peterson Law</a></small>
 
                     <hr/>
                     <p>He is an excellent website developer and android app developer. He was able to work with tight deadlines, and can do more than what you expect him to do for you!<br/><br/>Anyone is lucky to have Nathanne on their side!</p>
                 </div>
 
                 <div class="col-lg-4 p-4" align="center">
-                    <a target="_blank" href="https://www.instagram.com/ngenetttt/">
-                        <img src="@/assets/images/testimonials/kenneth.png" class="w-50" />
+                    <a target="_blank" href="https://www.instagram.com/bryy.em/">
+                        <img src="@/assets/images/testimonials/bryan.png" class="rounded-circle w-50" />
                     </a>
 
                     <h4 class="pt-4 mb-0">Mark Kenneth</h4>
-                    <small>BS Computer Science Student</small>
+                    <small>BS Computer Science Graduate</small>
 
                     <hr/>
-                    <p>Nathanne is an outstanding developer who provided exceptional services for my project. His expertise and professionalism made the entire process a pleasure, and I would highly recommend him to anyone looking for a top-notch developer.</p>
+                    <p>Nathanane Isip's programming skills are truly exceptional. His ability to tackle complex problems, write clean code, and collaborate effectively makes him a valuable asset to any team. I highly recommend him for any programming project.</p>
                 </div>
             </div>
         </div>
