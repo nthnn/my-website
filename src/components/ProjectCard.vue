241ef7b392ca4195314e272d8324dd521d0358fa
Nathanne Isip <nathanneisip@gmail.com>
2025-07-19 01:58:18 +0800

    Rendering tags as badge via ProjectCard component.

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,6 +16,18 @@ const router = useRouter();
 const goToProjectDetail = ()=> {
     router.push({ name: 'view', query: { id: props.item.id } });
 };
+
+function capitalizeFirstLetter(str: string): string {
+    return str.length === 0 ? "" :
+        str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+props.item.category.forEach((cat, index)=> {
+    if(cat == "ai")
+        props.item.category[index] = "AI";
+    else props.item.category[index] =
+        capitalizeFirstLetter(cat);
+});
 </script>
 
 <template>
@@ -29,6 +41,13 @@ const goToProjectDetail = ()=> {
         <div class="px-4 py-2">
             <h3 class="card-title mt-2 mb-0 pb-0">{{ props.item.title }}</h3>
             <small><i>{{ props.item.short_description }}</i></small>
+
+            <div class="d-flex flex-wrap gap-1 mt-2 mb-4">
+                <span v-for="cat in props.item.category" :key="cat" class="badge bg-info">
+                    {{ cat }}
+                </span>
+            </div>
+
             <p class="mt-2">{{ props.item.description }}</p>
         </div>
     </div>
